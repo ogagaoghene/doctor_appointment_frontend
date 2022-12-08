ce99c2769cd1b1520f81b7dc9b75be58264b10ee
Tarek <83262890+tarek102@users.noreply.github.com>
2022-12-08 13:58:13 +0200

    Create splash, signin and signup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import SplashScreen from './components/authorization/splash_screen';
+import Signin from './components/authorization/sign_in';
+import Signup from './components/authorization/sign_up';
 
 function App() {
   return (
     <div className="App">
+      
+        <Router>
+          <Routes>
+            <Route path='/' element={<SplashScreen />} />
+            <Route path="/sign_up" element={<Signup />} />
+            <Route path="/sign_in" element={<Signin />} />
+          </Routes>
+        </Router>
       
     </div>
   );
---
+++ src/components/authorization/splash_screen.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+const SplashScreen = () => {
+  return (
+    <div className='container-fluid d-flex flex-column justify-content-center align-items-center py-5'>
+      <div>
+        <h1 className=''>Book an Appointment now</h1>
+      </div>
+      <div className='btn-container'>
+        <Link className="h4 btn btn-primary m-3" to="/sign_up">Sign up</Link>
+        <Link className="h4 btn btn-secondary m-3" to="/sign_in">Sign in</Link>
+      </div>
+    </div>
+  )
+}
+
+export default SplashScreen;
